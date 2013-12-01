05cf88752be44d48228df1ab69c53f432111fec5
harvy <harvy@T510.(none)>
2013-12-01 15:55:16 +0400

    modified:   hdd-test-1-stage.sh 	modified:   hdd-test-2-stage.sh

--- hdd-test-1-stage.sh
+++ hdd-test-1-stage.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-expander=$(sg_map -x | awk '{if ( $6 == 13 ) { print $1 }}')
-api_url="http://$SERVER_NAME"
+	expander=$(sg_map -x | awk '{if ( $6 == 13 ) { print $1 }}')
+	api_url="http://$SERVER_NAME"
 
-max_pend=10    # Maximum Pending sectors
-max_offunc=10  # Maximum Offline UNC errors
-max_revent=10  # Maximim Realloceted events
-max_rsect=3     # Maximim Reallocated sectors (Bad blocks)
-max_read_err=2 # Maximum Rear Raw errors (for WD and Hitachi only)
+	max_pend=10    # Maximum Pending sectors
+	max_offunc=10  # Maximum Offline UNC errors
+	max_revent=10  # Maximim Realloceted events
+	max_rsect=3     # Maximim Reallocated sectors (Bad blocks)
+	max_read_err=2 # Maximum Rear Raw errors (for WD and Hitachi only)
 
-export expander
-export api_url max_pend max_offunc max_revent max_rsect
+	export expander
+	export api_url max_pend max_offunc max_revent max_rsect
 
 if [ -z $expander ]; then
-  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-  echo "JBOD is not connected, please connect the JBOD to this server"
-  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-  exit 1
+	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	echo "JBOD is not connected, please connect the JBOD to this server"
+	echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	exit 1
 fi 
 
 #############################===LIFGH_DOWN===###############################
@@ -27,22 +27,22 @@ light_down_slot ()
 {
 
 while :; 
-do
- if [ $(sg_ses -p 0x2 "$expander" | grep "Hot spare" -B 1 | grep "status: OK" -c) -eq 0 ] 
-    then
-     	for i in `seq 0 23`
-    do 
-	sg_ses --index="$i" --set=2:1=0 "$expander" 2>/dev/null 
-	sg_ses --index="$i" --set=3:5=0 "$expander" 2>/dev/null
-    done     
-     	dmesg -c 1>/dev/null
- fi
- sleep 10
-done
+	do
+ 	 if [ $(sg_ses -p 0x2 "$expander" | grep "Hot spare" -B 1 | grep "status: OK" -c) -eq 0 ] 
+    	 then
+	  	for i in `seq 0 23`
+          do 
+		sg_ses --index="$i" --set=2:1=0 "$expander" 2>/dev/null 
+		sg_ses --index="$i" --set=3:5=0 "$expander" 2>/dev/null
+    	  done     
+     		dmesg -c 1>/dev/null
+ 	 fi
+       		sleep 10
+	done
 }
-export -f light_down_slot
+	export -f light_down_slot
 
-light_down_slot &
+	light_down_slot &
 
 #############################===SMART_TEST===###############################
 smart_test ()
@@ -71,15 +71,15 @@ END {
  
 erase ()                                                                       
 {
-timeout 10 fdisk /dev/$block_dev <<EOF
+	timeout 10 fdisk /dev/$block_dev <<EOF
 o
 w
 EOF
 
-sectors=$(($(fdisk -s /dev/$block_dev)-102400))
+	sectors=$(($(fdisk -s /dev/$block_dev)-102400))
  
-timeout 15 dd if=/dev/zero of=/dev/$block_dev bs=1M count=100
-timeout 15 dd if=/dev/zero of=/dev/$block_dev bs=1k seek=$sectors count=102400
+	timeout 15 dd if=/dev/zero of=/dev/$block_dev bs=1M count=100
+	timeout 15 dd if=/dev/zero of=/dev/$block_dev bs=1k seek=$sectors count=102400
 
 }
 
@@ -89,24 +89,26 @@ timeout 15 dd if=/dev/zero of=/dev/$block_dev bs=1k seek=$sectors count=102400
 main ()
      
 {
-eval `smart_test /dev/$block_dev` 
+	eval `smart_test /dev/$block_dev` 
 
-wd=$(echo "$model" | grep WDC)
-hitachi=$(echo "$model" | grep -i Hitachi)
+	wd=$(echo "$model" | grep WDC)
+	hitachi=$(echo "$model" | grep -i Hitachi)
 
 
-data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
+	data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
 
-if [ ${rsec:-0} -gt $max_rsect -o ${pend:-0} -gt $max_pend -o ${offunc:-0} -gt $max_offunc -o ${revent:-0} -gt $max_revent -o ${health:-0} != "PASSED" -o -n "$ata_err" ] || [ -z $model -a -z $serial ] || [ -n "$wd" -o -n "$hitachi" -a ${read_error:-0} -gt $max_read_err ]; then
+	if [ ${rsec:-0} -gt $max_rsect -o ${pend:-0} -gt $max_pend -o ${offunc:-0} -gt $max_offunc -o ${revent:-0} -gt $max_revent \
+	-o ${health:-0} != "PASSED" -o -n "$ata_err" ] || [ -z $model -a -z $serial ] || [ -n "$wd" -o -n "$hitachi" -a ${read_error:-0} -gt $max_read_err ]
+	then
 
-	curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=SMART_FAILED&$data" "$api_url"
-	sg_ses --index="$slot" --set=3:5=1 "$expander"
-        erase $block_dev > /dev/null 2>&1
-        exit 0
-else
-        sg_ses --index="$slot" --set=2:1=1 "$expander"
+		curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=SMART_FAILED&$data" "$api_url"
+		sg_ses --index="$slot" --set=3:5=1 "$expander"
+        	erase $block_dev > /dev/null 2>&1
+        	exit 0
+	else
+        	sg_ses --index="$slot" --set=2:1=1 "$expander"
      	exit 0
-fi 2>/dev/null
+	fi 2>/dev/null
 
 }
 #############################===PRE-MAIN===#####################################
@@ -128,18 +130,18 @@ test_fn ()
 
   	data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err) 
    
-if [ -z "$block_dev" ]; then
-    	curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=NO_BLOCK_DEV&$data" "$api_url"
-	sg_ses --index="$slot" --set=3:5=1 "$expander"
-      	exit 0
-elif [ -n "$error_msg" ]; then
-    	curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=ERR_IN_DMESG&$data" "$api_url"
-     	sg_ses --index="$slot" --set=3:5=1 "$expander"
-      	erase $block_dev
-       	exit 0
-else
-    	main $block_dev
-fi
+	if [ -z "$block_dev" ]; then
+    		curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=NO_BLOCK_DEV&$data" "$api_url"
+		sg_ses --index="$slot" --set=3:5=1 "$expander"
+      		exit 0
+	elif [ -n "$error_msg" ]; then
+    		curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=ERR_IN_DMESG&$data" "$api_url"
+     		sg_ses --index="$slot" --set=3:5=1 "$expander"
+      		erase $block_dev
+       		exit 0
+	else
+    		main $block_dev
+	fi
 
 }
 
@@ -147,18 +149,18 @@ fi
 lock_fn ()
 {
 (
-[ -n "$1" ] || exit 0
+	[ -n "$1" ] || exit 0
 	flock -n /dev/$1 -c "test_fn $1"
    	sleep 5
 ) &
 }
 
 #############################===START_HERE===##############################
-export SHELL
-export -f test_fn
-export -f main
-export -f lock_fn
-export -f smart_test
-export -f erase
+	export SHELL
+	export -f test_fn
+	export -f main
+	export -f lock_fn
+	export -f smart_test
+	export -f erase
 
-inotifywait -mr -e create /dev/  | while read line; do lock_fn $(echo $line | grep -o -E sg[0-9]+ ) ;done
+	inotifywait -mr -e create /dev/  | while read line; do lock_fn $(echo $line | grep -o -E sg[0-9]+ ) ;done
--- hdd-test-2-stage.sh
+++ hdd-test-2-stage.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-expander=$(sg_map -x | awk '{if ( $6 == 13 ) { print $1 }}')
-api_url="http://$SERVER_NAME"
+set -e
+	expander=$(sg_map -x | awk '{if ( $6 == 13 ) { print $1 }}')
+	api_url="http://$SERVER_NAME"
 
-max_pend=10    # Maximum Pending sectors
-max_offunc=10  # Maximum Offline UNC errors
-max_revent=10  # Maximim Reallocetedents
-max_rsec=3     # Maximim Reallocated sectors (Bad blocks)
-
-
-export expander
-export api_url max_pend max_offunc max_revent max_rsec
+	max_pend=10    # Maximum Pending sectors
+	max_offunc=10  # Maximum Offline UNC errors
+	max_revent=10  # Maximim Reallocetedents
+	max_rsec=3     # Maximim Reallocated sectors (Bad blocks)
 
 
-if [ -z $expander ]; then
-  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-  echo "JBOD is not connected, please connect the JBOD to this server"
-  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-  exit 1
-fi 
+	export expander
+	export api_url max_pend max_offunc max_revent max_rsec
+
+
+	if [ -z $expander ]; then
+		echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+		echo "JBOD is not connected, please connect the JBOD to this server"
+		echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+		exit 1
+	fi 
 
 #############################===SMART_COLLECT===###############################
 smart_test ()
@@ -46,23 +47,23 @@ END {
 smart_chk ()
            
 {
-eval `smart_test /dev/$block_dev`
+	eval `smart_test /dev/$block_dev`
 
-wd=$(echo "$model" | grep WDC)
-hitachi=$(echo "$model" | grep -i Hitachi)
+	wd=$(echo "$model" | grep WDC)
+	hitachi=$(echo "$model" | grep -i Hitachi)
 
-data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
+	data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
 
-if [ ${rsec:-0} -gt $max_rsect -o ${pend:-0} -gt $max_pend -o ${offunc:-0} -gt $max_offunc -o ${revent:-0} -gt $max_revent -o ${health:-0} != "PASSED" -o -n "$ata_err" ] || [ -z $model -a -z $serial ] || [ -n "$wd" -o -n "$hitachi" -a ${read_error:-0} -gt $max_read_err ]; then
-        curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=SMART_AFTER_BB_FAILED&$data" "$api_url"
-        sg_ses --index="$slot" --set=3:5=1 "$expander"
-        exit 0
-else
-        curl -v -d "STAGE=1&RESULT=VALID&REASON=BB_and_SMART_PASSED&$data" "$api_url" 
-        sg_ses --index="$slot" --set=2:1=1 "$expander"
-        exit 0
-fi 2>/dev/null
-
+	if [ ${rsec:-0} -gt $max_rsect -o ${pend:-0} -gt $max_pend -o ${offunc:-0} -gt $max_offunc -o ${revent:-0} -gt $max_revent \
+	-o ${health:-0} != "PASSED" -o -n "$ata_err" ] || [ -z $model -a -z $serial ] || [ -n "$wd" -o -n "$hitachi" -a ${read_error:-0} -gt $max_read_err ]; then
+        	curl -v -g -d "STAGE=1&RESULT=FAILED&REASON=SMART_AFTER_BB_FAILED&$data" "$api_url"
+        	sg_ses --index="$slot" --set=3:5=1 "$expander"
+        	exit 0
+	else
+        	curl -v -d "STAGE=1&RESULT=VALID&REASON=BB_and_SMART_PASSED&$data" "$api_url" 
+        	sg_ses --index="$slot" --set=2:1=1 "$expander"
+        	exit 0
+	fi 2>/dev/null
 }
 
 
@@ -71,34 +72,35 @@ fi 2>/dev/null
 bblock ()
 {
 
-eval `smart_test /dev/$block_dev`
+	eval `smart_test /dev/$block_dev`
 
-bblock_fail="Too many bad blocks, aborting test"
-bblock_passed="Pass completed, 0 bad blocks found. (0/0/0 errors)"
+	bblock_fail="Too many bad blocks, aborting test"
+	bblock_passed="Pass completed, 0 bad blocks found. (0/0/0 errors)"
 
-data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
+	data=$(echo "MODEL="$model"&SERIAL_NUM="$serial"&POH="$poh"&READ_ERROR_RATE="$read_error"&CURRENT_PEND=""$pend""&REALLOC=""$rsec""&OFFLINE_UNC=""$offunc""&REALL_EVENT=""$revent""&HEALTH="$health"&ATA_ERROR="$ata_err)
 
 run_badblocks ()
+
 {
-           badblocks -wv -b 512 -e 3 -s -t 0x55 -t 0xff  /dev/$block_dev
+	badblocks -wv -b 512 -e 3 -s -t 0x55 -t 0xff  /dev/$block_dev
 
 }
 
-	   run_badblocks $block_dev 2>&1 tee |  while read line;do
-
-case $line in
-	$bblock_fail )
-	   curl -v -g -d "STAGE=2&RESULT=FAILED&REASON=BBLOCK_FAILED&$data" "$api_url"
- 	   sg_ses --index="$slot" --set=3:5=1 "$expander"
-           exit 0
+	run_badblocks $block_dev 2>&1 tee |  while read line
+	do
+	case $line in
+		$bblock_fail )
+		curl -v -g -d "STAGE=2&RESULT=FAILED&REASON=BBLOCK_FAILED&$data" "$api_url"
+ 		sg_ses --index="$slot" --set=3:5=1 "$expander"
+		exit 0
         ;;
-        $bblock_passed )
-	   curl -v -d "RESULT=VALID&$data" "$SERVER"
-           smart_chk $block_dev
-	   exit 0
+	        $bblock_passed )
+		curl -v -d "RESULT=VALID&$data" "$SERVER"
+	        smart_chk $block_dev
+		exit 0
         ;;
-esac
-done
+	esac
+	done
 }
 #############################===PRE-MAIN===#####################################
 
@@ -124,17 +126,17 @@ test_fn ()
 lock_fn ()
 {
 (
-[ -n "$1" ] || exit 0
-	  flock -n /dev/$1 -c "test_fn $1"
-   	  sleep 5
+	[ -n "$1" ] || exit 0
+	flock -n /dev/$1 -c "test_fn $1"
+	sleep 5
 ) &
 }
 
 #############################===START_HERE===##############################
-export SHELL
-export -f lock_fn test_fn
-export -f bblock
-export -f smart_chk
-export -f smart_test
+	export SHELL
+	export -f lock_fn test_fn
+	export -f bblock
+	export -f smart_chk
+	export -f smart_test
 
-inotifywait -mr -e create /dev/  | while read line; do lock_fn $(echo $line | grep -o -E sg[0-9]+ ) ;done
+	inotifywait -mr -e create /dev/  | while read line; do lock_fn $(echo $line | grep -o -E sg[0-9]+ ) ;done
